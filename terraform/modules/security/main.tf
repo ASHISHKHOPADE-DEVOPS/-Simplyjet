f5c5903cec8ef2ff4ef6023b5a3ef84ef9386b5a
Ashish  Khopade <khopadeashish600@gmail.com>
2024-09-14 13:23:23 +0530

    Create main.tf

---
+++ terraform/modules/security/main.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "eks_sg" {
+  vpc_id = aws_vpc.main.id
+  description = "EKS Security Group"
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "eks_role" {
+  name = "eks_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
