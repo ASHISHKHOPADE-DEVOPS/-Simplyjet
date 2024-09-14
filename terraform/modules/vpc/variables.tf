9d78fd3a858b7bcfbb997ea02b4beee6515772e7
Ashish  Khopade <khopadeashish600@gmail.com>
2024-09-14 20:48:19 +0530

    Update variables.tf

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,14 +1,29 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
 variable "vpc_cidr" {
-  type = string
-  default = "10.0.0.0/16"
+  description = "VPC CIDR block"
+  type        = string
 }
 
-variable "public_subnets_cidrs" {
-  type = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "public_subnet_count" {
+  description = "Number of public subnets"
+  type        = number
 }
 
-variable "private_subnets_cidrs" {
-  type = list(string)
-  default = ["10.0.3.0/24", "10.0.4.0/24"]
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_count" {
+  description = "Number of private subnets"
+  type        = number
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
 }
