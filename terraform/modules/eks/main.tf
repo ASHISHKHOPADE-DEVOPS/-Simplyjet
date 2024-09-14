eff77bd07b31a02122e61da136b289143c8da8b7
Ashish  Khopade <khopadeashish600@gmail.com>
2024-09-14 20:49:03 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,27 +1,83 @@
 provider "aws" {
-  region = "us-west-2"
+  region = var.region
 }
 
 resource "aws_eks_cluster" "main" {
-  name     = "my-cluster"
+  name     = var.cluster_name
   role_arn  = aws_iam_role.eks.arn
   version   = "1.21"
-
+  
   vpc_config {
-    subnet_ids = aws_subnet.public[*].id
+    subnet_ids = var.subnet_ids
+  }
+  
+  tags = {
+    Name = "main-eks-cluster"
   }
 }
 
-# Node group configuration
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_role_arn   = aws_iam_role.node.arn
-  subnet_ids       = aws_subnet.public[*].id
+  subnet_ids       = var.subnet_ids
   scaling_config {
-    desired_size = 2
-    max_size     = 5
-    min_size     = 1
+    desired_size = var.node_desired_size
+    max_size     = var.node_max_size
+    min_size     = var.node_min_size
+  }
+  
+  tags = {
+    Name = "main-eks-node-group"
   }
 }
 
-# IAM roles and other configurations
+resource "aws_iam_role" "eks" {
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks" {
+  role       = aws_iam_role.eks.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "node" {
+  name = "eks-node-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node" {
+  role       = aws_iam_role.node.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.main.name
+}
+
+output "node_group_name" {
+  value = aws_eks_node_group.main.name
+}
+
